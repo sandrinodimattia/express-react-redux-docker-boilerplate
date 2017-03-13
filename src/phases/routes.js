8333d23208f9025923595ccbe25ebb8946afc966
Sandrino Di Mattia <contact@sandrino.be>
2017-03-13 14:02:54 +0100

    Added vendor styles and config object in the view

--- src/phases/routes.js
+++ src/phases/routes.js
@@ -2,8 +2,12 @@ module.exports = function phase() {
   this.get('*', (req, res) => {
     res.render('index', {
       title: 'My App',
+      config: {
+        foo: 'bar'
+      },
       style: {
-        client: res.locals.webpack_asset('client').css
+        client: res.locals.webpack_asset('client').css,
+        vendor: (res.locals.webpack_asset('vendors') && res.locals.webpack_asset('vendors').css) || null
       },
       scripts: {
         client: res.locals.webpack_asset('client').js,
